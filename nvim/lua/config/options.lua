b194cf5050176c7618670c1a8eb08675d3b1bde1
2salted <noamgraf123@gmail.com>
2024-04-06 14:03:20 +0000

    added nvim config

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.g.R_external_term = 1
+vim.g.lazyvim_python_lsp = "basedpyright"
+vim.o.scrolloff = 999
